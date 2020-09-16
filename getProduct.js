3cd1e63b21e93a73b46e54eb0be913498466266c
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2020-09-16 09:29:23 +0200

    add js

---
+++ getProduct.js
@@ -0,0 +1,33 @@
+console.log('get Menu & pruduct');
+
+fetch('./assets/Wacdonald/Wacdonald/menu.json')
+  .then(response => response.json())
+  .then(data => {
+  	// Do something with your data
+    console.log(data);
+    menu = data;
+    menu.map(menu => {
+        
+        let menuelement ="";
+        let productelement ="";
+
+        menu.cat==="menu" ? menuelement = `
+        <div class="card-menu-item">
+                    <img src="/assets/Wacdonald/Wacdonald/icons/`+menu.icon+`"/>
+                    <div>`+menu.name+`</div>
+                </div>
+        ` : '' ;
+
+        menu.cat==="product" ? productelement = `
+        <div class="card-menu-item">
+                    <img src="/assets/Wacdonald/Wacdonald/icons/`+menu.icon+`"/>
+                    <div>`+menu.name+`</div>
+                </div>
+        ` : '' ;
+
+            document.getElementById("menu").insertAdjacentHTML('beforeend', menuelement);
+            document.getElementById("product").insertAdjacentHTML('beforeend', productelement);
+        });
+
+        
+    });
