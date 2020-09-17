fb8e025361db314b02841ad8de848c1d15ced0e6
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2020-09-17 16:26:10 +0200

    js changes

--- getProduct.js
+++ getProduct.js
@@ -1,25 +1,24 @@
 console.log('get Menu & pruduct');
-
+let orderedElements = [];
 fetch('./assets/Wacdonald/Wacdonald/menu.json')
   .then(response => response.json())
   .then(data => {
   	// Do something with your data
     console.log(data);
-    menu = data;
-    menu.map(menu => {
-        
+    let menus = data;
+    menus.map(menu => {
         let menuelement ="";
         let productelement ="";
 
         menu.cat==="menu" ? menuelement = `
-        <div class="card-menu-item" id="`+menu.id+`" >
+        <div class="card-menu-item" id="`+menu.id+`" key="`+menu.key+`">
                     <img src="/assets/Wacdonald/Wacdonald/icons/`+menu.icon+`"/>
                     <div>`+menu.name+`</div>
                 </div>
         ` : '' ;
 
         menu.cat==="product" ? productelement = `
-        <div class="card-menu-item" key="" >
+        <div class="card-menu-item" id="`+menu.id+`">
                     <img src="/assets/Wacdonald/Wacdonald/icons/`+menu.icon+`"/>
                     <div>`+menu.name+`</div>
                 </div>
@@ -28,4 +27,36 @@ fetch('./assets/Wacdonald/Wacdonald/menu.json')
             document.getElementById("menu").insertAdjacentHTML('beforeend', menuelement);
             document.getElementById("product").insertAdjacentHTML('beforeend', productelement);
         });
-    });
+        
+        let elements = document.getElementsByClassName("card-menu-item");
+        console.log(elements);
+        Array.from(elements).forEach(function(element){
+            element.addEventListener("mouseenter",function(){
+                //console.log("enter");
+                element.className = "clicked-card-menu-item";
+            });
+            element.addEventListener("mouseleave",function(){
+                //console.log("leave");
+                element.className = "card-menu-item";
+            });
+            element.addEventListener("click",function(){
+                console.log(element.id);
+                let selected = menus.find(menu => menu.id == element.id);
+                console.log(selected);
+                 let orderelement = `
+                    <div class="order-item">
+                        <img src="/assets/Wacdonald/Wacdonald/icons/`+selected.icon+`"/>
+                        <div class="item-title">`+selected.name+`</div>
+                        <div class="item-price">$ `+selected.price+`</div>
+                        <div class="item-counter">
+                            <button class="add-button" value="-">-</button>
+                            <div class="counter">1</div>
+                            <button class="en-button" value="+">+</button>
+                    </div>
+                `;
+                document.getElementById("orderlist").insertAdjacentHTML("beforeend", orderelement);
+            });
+        });
+       
+});
+    
