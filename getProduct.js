58508790eae653d6bda61813638dd9f261d9012b
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2020-09-18 11:47:51 +0200

    js changes

--- getProduct.js
+++ getProduct.js
@@ -31,7 +31,7 @@ fetch('./assets/Wacdonald/Wacdonald/menu.json')
         });
         
         let elements = document.getElementsByClassName("card-menu-item");
-        console.log(elements);
+        
         Array.from(elements).forEach(function(element){
             element.addEventListener("mouseenter",function(){
                 //console.log("enter");
@@ -42,20 +42,22 @@ fetch('./assets/Wacdonald/Wacdonald/menu.json')
                 element.className = "card-menu-item";
             });
             element.addEventListener("click",function(){
-                console.log(element.id);
+                
                 let menuSelected = menus.find(menu => menu.id == element.id);
-                console.log(menuSelected);
-                orderedElements.includes(menuSelected) ?  "" : orderedElements.push(menuSelected);
-                //orderedElements.push(menuSelected);
+               
+                
+                orderedElements.includes(menuSelected) ? "" : orderedElements.push(menuSelected);
+
                 console.log(orderedElements);
-                refreshOrder();
+                addItemToOder();
             });
         });
 });
 
-function refreshOrder(){
+function addItemToOder(){
     document.getElementById("orderlist").innerHTML ="";
-    console.log("refrech");
+    console.log("addItemToOder");
+    let price = 0;
     orderedElements.map( order => {
         let orderelement = `
                     <div class="order-item">
@@ -63,12 +65,56 @@ function refreshOrder(){
                         <div class="item-title">`+order.name+`</div>
                         <div class="item-price">$ `+order.price+`</div>
                         <div class="item-counter">
-                            <button class="add-button" value="-">-</button>
-                            <div class="counter">1</div>
-                            <button class="en-button" value="+">+</button>
+                            <button class="en-button" id="en-button-`+order.id+`" value="-">-</button>
+                            <div class="counter" id="counter-`+order.id+`" value="1">1</div>
+                            <button class="add-button" id="add-button-`+order.id+`" value="+">+</button>
                     </div>
                 `;
-        document.getElementById("orderlist").insertAdjacentHTML("beforeend", orderelement);
-    });
+                document.getElementById("orderlist").insertAdjacentHTML("beforeend", orderelement);
+                    
+        // up order price
+       
+        price = price + order.price;
+        totalNode = document.getElementById("total-price");
+        totalNode.innerHTML = "$"+price;
+        totalNode.setAttribute("value", price);
+        
+        //adding the function buttons - 1 +
+        let addButton = document.getElementById(`add-button-`+order.id);
+        let enButton = document.getElementById(`en-button-`+order.id);
+
+        addButton.addEventListener("click",function(){
+            let counterValue = parseInt(document.getElementById(`counter-`+order.id).getAttribute("value"));
+            console.log(counterValue);
+            counterValue = counterValue + 1;
+            document.getElementById(`counter-`+order.id).setAttribute("value",counterValue);
+            document.getElementById(`counter-`+order.id).innerHTML = counterValue;
+
+            //update price
+            price = price + order.price;
+            totalNode = document.getElementById("total-price");
+            totalNode.innerHTML = "$"+price;
+            totalNode.setAttribute("value", price);
+
+        });
+
+        enButton.addEventListener("click",function(){
+            let counterValue = parseInt(document.getElementById(`counter-`+order.id).getAttribute("value"));
+            if (counterValue > 0 ){
+                console.log(counterValue);
+                counterValue = counterValue - 1;
+                document.getElementById(`counter-`+order.id).setAttribute("value",counterValue);
+                document.getElementById(`counter-`+order.id).innerHTML = counterValue;
+
+                //update item price
+                price = price - order.price;
+                totalNode = document.getElementById("total-price");
+                totalNode.innerHTML = "$"+price;
+                totalNode.setAttribute("value", price);
+                //update order price
+
+            }
+        });
+    })
 }
     
